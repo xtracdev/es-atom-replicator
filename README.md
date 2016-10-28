99fe79403405238cbd10c636dfc19c61271e51e4
d-smith <doug.smith.mail@gmail.com>
2016-10-28 12:45:47 -0700

    Put first test skeleton in place

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ replicated from a remote instance into the local instance.
 To simplify processing, each replication action, be it the 'catch up' 
 phase or the reading of recent events, will be performed after 
 acquiring a mutex via a table lock.
+
+The table use for the lock is 
+
+<pre>
+create table replicator_lock (
+    ts TIMESTAMP
+);
+</pre>
+
---
+++ internal/features/replication/concurrency_steps.go
@@ -0,0 +1,31 @@
+package replication
+
+import . "github.com/gucumber/gucumber"
+
+func init() {
+	Given(`^two replicators$`, func() {
+		T.Skip() // pending
+	})
+
+	When(`^both are started$`, func() {
+		T.Skip() // pending
+	})
+
+	Then(`^only one may execute the catch up logic$`, func() {
+		T.Skip() // pending
+	})
+
+	When(`^start up is complete and steady state is reached$`, func() {
+		T.Skip() // pending
+	})
+
+	And(`^there are recent events$`, func() {
+		T.Skip() // pending
+	})
+
+	Then(`^only one replicator processes the recent events$`, func() {
+		T.Skip() // pending
+	})
+
+}
+
