255543d33066dcbc42fbb86876e9bddfdef1f1a8
d-smith <doug.smith.mail@gmail.com>
2016-10-30 07:27:18 -0700

    Added some test skeletons

---
+++ replicator_test.go
@@ -0,0 +1,19 @@
+package replicator
+
+import "testing"
+
+func TestReplicateFromScratch(t *testing.T) {
+
+}
+
+func TestReplicateFromMidFeed(t *testing.T) {
+
+}
+
+func TestReplicateWithNewFeedAdded(t *testing.T) {
+
+}
+
+func TestReplicateEmpty(t *testing.T) {
+	
+}
