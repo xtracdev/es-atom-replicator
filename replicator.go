25d8193070b858c3af0d56c9175e2e082049949e
d-smith <doug.smith.mail@gmail.com>
2016-10-31 08:48:34 -0700

    Empty replication unit test

--- replicator.go
+++ replicator.go
@@ -1,18 +1,21 @@
 package replicator
 
 import (
-	"golang.org/x/tools/blog/atom"
 	"database/sql"
+	"errors"
+	log "github.com/Sirupsen/logrus"
+	"golang.org/x/tools/blog/atom"
+	"strings"
 )
 
 type Locker interface {
-	GetLock()(bool, error)
+	GetLock(lockerArgs ...interface{}) (bool, error)
 	ReleaseLock() error
 }
 
 type FeedReader interface {
 	GetRecent() (*atom.Feed, error)
-	GetFeed(feedid string)(*atom.Feed, error)
+	GetFeed(feedid string) (*atom.Feed, error)
 }
 
 type Replicator interface {
@@ -20,29 +23,93 @@ type Replicator interface {
 }
 
 type ReplicatorFactory interface {
-	New(locker Locker, feedReader FeedReader, extras... interface{}) (Replicator, error)
+	New(locker Locker, feedReader FeedReader, extras ...interface{}) (Replicator, error)
 }
 
-
 type OraEventStoreReplicator struct {
-	db *sql.DB
-	locker Locker
+	db         *sql.DB
+	locker     Locker
 	feedReader FeedReader
 }
 
 func (r *OraEventStoreReplicator) ProcessFeed() error {
+
+	//Do the work in a transaction
+	log.Info("ProcessFeed start transaction")
+	tx, err := r.db.Begin()
+	if err != nil {
+		log.Warnf("Error starting transaction: %s", err.Error())
+		return err
+	}
+
+	//Get lock within the context of a transaction
+	locked, err := r.locker.GetLock(tx)
+	if err != nil {
+		log.Warnf("Error obtaining lock: %s", err.Error())
+		tx.Rollback()
+		return err
+	}
+
+	if !locked {
+		log.Info("ProcessFeed did not get lock... returning.")
+		tx.Rollback()
+		return nil
+	}
+
+	//What's the last event seen?
+
+	//Find the feed with the event
+
+	//If this is the last event in the feed, get the next
+	//feed.
+
+	//Add all the events in this feed that have not been added before
+
+	//Unlock
+
 	return nil
 }
 
-type OraEventStoreReplicatorFactory struct {}
+type OraEventStoreReplicatorFactory struct{}
 
-func (oesFact *OraEventStoreReplicatorFactory) New(locker Locker, feedReader FeedReader,db *sql.DB) (Replicator, error){
+func (oesFact *OraEventStoreReplicatorFactory) New(locker Locker, feedReader FeedReader, db *sql.DB) (Replicator, error) {
 	return &OraEventStoreReplicator{
-		db: db,
-		locker: locker,
+		db:         db,
+		locker:     locker,
 		feedReader: feedReader,
-	},nil
+	}, nil
 }
 
+type TableLocker struct{}
 
+var errGetLockArgument = errors.New("Table locker GetLock expects a single argument of type *sql.Tx")
 
+func (tl *TableLocker) GetLock(args ...interface{}) (bool, error) {
+	if len(args) != 1 {
+		return false, errGetLockArgument
+	}
+
+	tx, ok := args[0].(*sql.Tx)
+	if !ok {
+		return false, errGetLockArgument
+	}
+
+	log.Info("locking table replicator_lock")
+	_, err := tx.Exec("lock table replicator_lock nowait")
+	if err == nil {
+		log.Info("Acquired lock")
+		return true, nil
+	}
+
+	if strings.Contains(err.Error(), "ORA-00054") {
+		log.Info("Did not acquire lock")
+		return false, nil
+	} else {
+		log.Warnf("Error locking table: %s", err.Error())
+		return false, err
+	}
+}
+
+func (tl *TableLocker) ReleaseLock() error {
+	return nil //Release done in transaction commit/rollback
+}
--- replicator_test.go
+++ replicator_test.go
@@ -1,6 +1,53 @@
 package replicator
 
-import "testing"
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/tools/blog/atom"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
+	"testing"
+)
+
+var testFactory = &OraEventStoreReplicatorFactory{}
+
+func testExpectLock(mock sqlmock.Sqlmock, lockError bool) {
+	if lockError {
+		mock.ExpectExec("lock table replicator_lock nowait").WillReturnError(errors.New("BAM!"))
+	} else {
+		execOkResult := sqlmock.NewResult(1, 1)
+		mock.ExpectExec("lock table replicator_lock nowait").WillReturnResult(execOkResult)
+	}
+
+}
+
+type testFeedReader struct{}
+
+func (tfr *testFeedReader) GetRecent() (*atom.Feed, error) {
+	return nil, nil
+}
+
+func (tfr *testFeedReader) GetFeed(feedid string) (*atom.Feed, error) {
+	return nil, nil
+}
+
+func TestReplicateEmpty(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectBegin()
+	testExpectLock(mock, false)
+
+	replicator, err := testFactory.New(new(TableLocker), new(testFeedReader), db)
+	assert.Nil(t, err)
+
+	replicator.ProcessFeed()
+
+	err = mock.ExpectationsWereMet()
+	assert.Nil(t, err)
+}
 
 func TestReplicateFromScratch(t *testing.T) {
 
@@ -14,6 +61,6 @@ func TestReplicateWithNewFeedAdded(t *testing.T) {
 
 }
 
-func TestReplicateEmpty(t *testing.T) {
-	
+func TestReplicateCantLock(t *testing.T) {
+
 }
